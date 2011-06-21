35a71bb5623c52c7679a7da8c3d940127a0d794d
Gram Gibson <gram.gibson@gmail.com>
2011-06-21 10:16:44 -0400

    Add looping through strongly-typed collection example

---
+++ Nustache.Mvc.Example/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nustache.Mvc.Example.Models 
+{
+	public class Post 
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+	}
+}
