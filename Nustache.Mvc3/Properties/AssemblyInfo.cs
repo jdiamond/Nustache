847c65e6debef472be7ab77be958eeee79758b02
Jason Diamond <jason@diamond.name>
2012-02-10 20:52:05 -0800

    Added nuspec and HISTORY.txt files.

--- Nustache.Mvc3/Properties/AssemblyInfo.cs
+++ Nustache.Mvc3/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nustache.Mvc3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Logic-less templates for ASP.NET MVC 3")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jason Diamond")]
 [assembly: AssemblyProduct("Nustache.Mvc3")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.12.2.10")]
+[assembly: AssemblyFileVersion("1.12.2.10")]
