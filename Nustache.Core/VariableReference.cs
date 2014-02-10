02d86937d1bc8812314dd404708a3a4815bd6a5f
robertvs <robertvs@gmail.com>
2014-02-10 09:58:38 -0600

    Added value formatter to "pretty print" objects

--- Nustache.Core/VariableReference.cs
+++ Nustache.Core/VariableReference.cs
@@ -48,6 +48,8 @@ namespace Nustache.Core
             }
             else if (value != null)
             {
+                if (ValueFormatter.HasRegisteredFormatters)
+                    value = ValueFormatter.Format(value);
                 context.Write(_escaped
                     ? Encoders.HtmlEncode(value.ToString())
                     : value.ToString());
