d2f937f2ecc42023b1df587586cf002b69e89a55
robertvs <robertvs@gmail.com>
2014-02-10 09:51:20 -0600

    Added valueformatter to nustache to be able to manipulate how a certain type should render

---
+++ Nustache.Core.Tests/Describe_ValueFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Nustache.Core.Tests
+{
+    [TestFixture]
+    public class Describe_ValueFormatter : FileSystemTestFixture
+    {
+        [Test]
+        public void It_can_format_object_with_value_formatter()
+        {
+            string decFormat = ".#"; //1 decimal
+            var model = new
+            {
+                price = (decimal)55.4545458m,
+            };
+
+            ValueFormatter.Add<decimal>(val => val.ToString(decFormat));
+
+            var output = Render.StringToString("{{price}}", model);
+            var priceString = model.price.ToString(decFormat);
+            Assert.AreEqual(priceString, output);
+        }
+
+    }
+}
---
+++ Nustache.Core/ValueFormatter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nustache.Core
+{
+    /// <summary>
+    /// Will format the the value with custom delegate
+    /// </summary>
+    public static class ValueFormatter
+    {
+        public delegate TResult Func<TInput, TResult>(TInput arg);
+        private static Dictionary<Type, Func<object, object>> _typeFormatters = new Dictionary<Type, Func<object, object>>();
+        private static bool _hasRegisteredFormatters;
+
+        /// <summary>
+        /// Gets whether the ValueFormatter has registered delegates, and if it should be used
+        /// </summary>
+        public static bool HasRegisteredFormatters
+        {
+            get
+            {
+                return _hasRegisteredFormatters;
+            }
+        }
+        /// <summary>
+        /// Adds a type and func to the collection
+        /// </summary>
+        /// <param name="typeFormatter">The delegate to execute on the object</param>
+        /// <typeparam name="T">The type to format</typeparam>
+        public static void Add<T>(Func<T, object> typeFormatter)
+        {
+            var type = typeof(T);
+            if (!_typeFormatters.ContainsKey(type))
+            {
+                Func<object, object> valueFormatter = value => typeFormatter((T)value);
+                _typeFormatters.Add(type, valueFormatter);
+                _hasRegisteredFormatters = true;
+            }
+        }
+        /// <summary>
+        /// removes the formatting function from the collection
+        /// </summary>
+        /// <param name="type"></param>
+        public static void Remove(Type type)
+        {
+            if (_typeFormatters.ContainsKey(type))
+            {
+                _typeFormatters.Remove(type);
+                if (_typeFormatters.Count == 0)
+                {
+                    _hasRegisteredFormatters = false;
+                }
+            }
+        }
+        /// <summary>
+        /// Formats an object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static object Format(object obj)
+        {
+            var type = obj.GetType();
+            var func = _typeFormatters[type];
+            if (func == null)
+                return obj;
+            try
+            {
+                return func(obj);
+            }
+            catch { return obj; }
+        }
+    }
+}
