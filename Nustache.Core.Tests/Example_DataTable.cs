d5eecbee290856faa872960855d379c22e6f9c0d
Romanx <mcauliffe.alex@gmail.com>
2015-04-05 12:14:41 +0100

    Added DataTable Examples and Render Test #79

---
+++ Nustache.Core.Tests/Example_DataTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nustache.Core.Tests
+{
+    [TestFixture]
+    public class Example_DataTable
+    {
+        [Test]
+        public void It_can_Render_Datatables()
+        {
+            var dt = new System.Data.DataTable();
+            dt.Columns.Add("IntColumn", typeof(int));
+            dt.Rows.Add(new object[] { 1 });
+            dt.Rows.Add(new object[] { 2 });
+            dt.Rows.Add(new object[] { 3 });
+
+            var result = Render.StringToString("{{#Item}}{{IntColumn}}{{/Item}}", new { Item = dt });
+
+            Assert.AreEqual("123", result);
+        }
+    }
+}
