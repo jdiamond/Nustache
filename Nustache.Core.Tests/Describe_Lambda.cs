0491971d3325560cb8cbf5305230c61cb08c2990
Romanx <mcauliffe.alex@gmail.com>
2015-04-25 11:52:40 +0100

    Moved the untyped delegate tests to Lambda tests
     #33

--- Nustache.Core.Tests/Describe_Lambda.cs
+++ Nustache.Core.Tests/Describe_Lambda.cs
@@ -15,5 +15,28 @@ namespace Nustache.Core.Tests
             });
             Assert.AreEqual("<b>Nustache is awesome.</b>", result);
         }
+
+        [Test]
+        public void It_can_render_nontyped_delegate_functions()
+        {
+            var result = Render.StringToString("{{foo}}, {{bar}}", new
+            {
+                foo = "bar",
+                bar = new System.Func<string>(() => { return "foo"; })
+            });
+
+            Assert.AreEqual("bar, foo", result);
+        }
+
+        [Test]
+        public void It_can_render_nontyped_delegate_functions_with_body()
+        {
+            var result = Render.StringToString("{{#wrapped}}{{name}} is awesome.{{/wrapped}}", new
+            {
+                wrapped = new System.Func<string, string>((body) => string.Format("<b>{0}</b>", body)),
+                name = "Nustache"
+            });
+            Assert.AreEqual("<b>Nustache is awesome.</b>", result);
+        }
     }
 }
