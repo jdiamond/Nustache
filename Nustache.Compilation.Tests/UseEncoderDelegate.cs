c1b98e72283af7d1c4f3b55e96578b55558e5f53
Yannic Staudt <yannic.staudt@marquardt.de>
2015-05-08 11:29:27 +0200

    Fixed bug in CompilePartVisitor that didn't rely on HtmlEncoder specified in the static Encoders class

---
+++ Nustache.Compilation.Tests/UseEncoderDelegate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Nustache.Compilation;
+using Nustache.Core;
+
+namespace Nustache.Compilation.Tests
+{
+    [TestFixture]
+    public class Encoder_Delegate_Usage
+    {
+        class TemplateData
+        {
+            public string Value { get; set; }
+        }
+
+        [Test]
+        public void ReplacingHtmlEncodeWorksForCompiledTemplates()
+        {
+            // replace the default encoder with one that wraps the input in "--" 
+            Encoders.HtmlEncoder encoder = (input) => "--" + input + "--";
+            Encoders.HtmlEncode = encoder;
+
+            var template = Template("{{Value}}");
+            var compiled = template.Compile<TemplateData>(null);
+
+
+            var inputText = "Some cool text";
+            var data = new TemplateData()
+            {
+                Value = inputText
+            };
+
+            var expectedOutput = encoder(inputText);
+
+            Assert.AreEqual(expectedOutput, compiled(data));
+
+            // reset the used HTML encoder to default
+            Encoders.HtmlEncode = Encoders.DefaultHtmlEncode;
+        }
+
+        private Template Template(string text)
+        {
+            var template = new Template();
+            template.Load(new StringReader(text));
+            return template;
+        }
+    }
+}
