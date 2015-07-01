9df033c0bdfebddd3589609471b769d2b90655c3
Benjamin Burgy <benjamin.burgy@liebherr.com>
2015-07-01 09:46:01 +0200

    feature/jValue: Refactor the JValueIdentifier class to be integrated into PropertyDescriptorValueGetterFactory class.

--- Nustache.Core/JValueIdentifier.cs
+++ Nustache.Core/JValueIdentifier.cs
@@ -1,39 +1,42 @@
-﻿using System;
+﻿#pragma warning disable 1584,1711,1572,1581,1580
+
+using System;
 
 namespace Nustache.Core
 {
     /// <summary>
-    ///     Service resonsible for identifying whether a value is a <see cref="Newtonsoft.Json.Linq.JValue" />, determining its type and determining whether
-    ///     the value can be used as a <see cref="Boolean"/>.
+    ///   Service resonsible for identifying whether a value is a <see cref="Newtonsoft.Json.Linq.JValue" />, determining its
+    ///   type and returns its value.
     /// </summary>
-    public sealed class JValueIdentifier
+    public static class JValueIdentifier
     {
         #region Public Members
 
         /// <summary>
-        ///     Determines whether the <paramref name="obj"/> is a <see cref="Newtonsoft.Json.Linq.JValue" />.
+        ///   Determines whether the <paramref name="obj" /> is a <see cref="Newtonsoft.Json.Linq.JValue" />.
         /// </summary>
         /// <param name="obj">
-        ///     The <see cref="Object"/> retrieved from the model.
+        ///   The <see cref="object" /> retrieved from the model.
         /// </param>
         /// <returns>
-        ///     <see langword="True"/> whether the <paramref name="obj"/> is a <see cref="Newtonsoft.Json.Linq.JValue" /> object otherwise <see langword="False"/>.
+        ///   <see langword="True" /> whether the <paramref name="obj" /> is a <see cref="Newtonsoft.Json.Linq.JValue" /> object
+        ///   otherwise <see langword="False" />.
         /// </returns>
-        public bool IsJValue(object obj)
+        public static bool IsJValue(object obj)
         {
             return obj.GetType().ToString().Equals("Newtonsoft.Json.Linq.JValue");
         }
 
         /// <summary>
-        ///     Determines whether the value can be used as a <see cref="Boolean"/> and returns its representation.
+        ///   Determines whether the value can be used as a <see cref="bool" /> and returns its representation.
         /// </summary>
         /// <param name="jValue">
-        ///     The <see cref="Object"/> retrieved from the model.
+        ///   The <see cref="object" /> retrieved from the model.
         /// </param>
         /// <returns>
-        ///     The representation of the <paramref name="jValue"/> as a <see cref="Boolean"/> otherwise <see langword="null"/>.
+        ///   The representation of the <paramref name="jValue" /> as a <see cref="bool" /> otherwise <see langword="null" />.
         /// </returns>
-        public bool? IsTruthy(object jValue)
+        public static object GetValue(object jValue)
         {
             var jValueType = jValue.GetType();
             var typeOfValue = jValueType.GetProperty("Type").GetValue(jValue, null).ToString();
@@ -45,17 +48,17 @@ namespace Nustache.Core
 
             if (typeOfValue == "String") // JTokenType.String
             {
-                return !string.IsNullOrEmpty(GetValue<string>(jValueType, jValue));
+                return GetValue<string>(jValueType, jValue);
             }
 
             if (typeOfValue == "Integer") // JTokenType.Integer
             {
-                return GetValue<Int64>(jValueType, jValueType) > 0;
+                return GetValue<Int64>(jValueType, jValueType);
             }
 
             if (typeOfValue == "Float") // JTokenType.Float
             {
-                return GetValue<double>(jValueType, jValueType) > 0;
+                return GetValue<double>(jValueType, jValueType);
             }
 
             return null;
