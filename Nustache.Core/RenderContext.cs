edd2dcee40fbf6a80f88c57a0ec6f943745cd3ef
Benjamin Burgy <benjamin.burgy@liebherr.com>
2015-07-01 09:52:06 +0200

    feature/jValue: Rollback modifications.

--- Nustache.Core/RenderContext.cs
+++ Nustache.Core/RenderContext.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
@@ -233,7 +234,7 @@ namespace Nustache.Core
             {
                 return !string.IsNullOrEmpty((string)value);
             }
-
+            
             if (value is IEnumerable)
             {
                 return ((IEnumerable)value).GetEnumerator().MoveNext();
